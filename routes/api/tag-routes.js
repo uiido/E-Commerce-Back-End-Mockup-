fdd6b7d888a92399934b580b1be10b5f66a4e162
uiido <uiido@yahoo.com>
2023-01-17 22:35:25 -0500

    Delete tags filled out in routers.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const { restart } = require('nodemon');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
@@ -46,6 +47,18 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.destroy({ where: { id: req.params.id } })
+    .then(tagData => {
+      if (!tagData) {
+        res.status(404).json({ message: "Sorry, no tag with this id was found!" });
+        return;
+      }
+      res.json({ message: "This product was successfully deleted!" });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    })
 });
 
 module.exports = router;
